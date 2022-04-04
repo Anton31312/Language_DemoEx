87a97c966469ef69da021f3d03ae00bd725c1b88
Anton31312 <kuropatkina.op@mail.ru>
2022-04-04 15:04:46 +0300

    000

---
+++ Fedyaev_Language_01/ClassHelper/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fedyaev_Language_01.EF
+{
+    public partial class Client
+    {
+        static int CountVisit
+        {
+            get
+            {
+                return ClientService.Count();
+            }
+            
+        }
+    }
+}
